d7221e716b3368c4aa262929be8a93f976bd934e
Anh Pham Hoang <anhph@devcamp.edu.vn>
2023-02-19 20:29:53 +0700

    đọc file XML và lên form hóa đơn điện tử

---
+++ WebApplication1/Models/Buyer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_ReadXML.Models
+{
+    public class Buyer
+    {
+        public string Ten { get; set; }
+        public string MST { get; set; }
+        public string DChi { get; set; }
+    }
+}
